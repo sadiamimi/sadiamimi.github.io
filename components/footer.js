2afada4ffb768af2100ec63366c23dccfd38f7a4
Sadia Afrose <95015966+sadiamimi@users.noreply.github.com>
2024-12-24 01:57:29 +0600

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -10,20 +10,18 @@ class Footer extends HTMLElement {
         <div class="py-3 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Your Name</p>
-                    <p class="my-0 pt-0"><a class="" href="mailto:yourname@email.org">yourname@email.org</a></p>
+                    <p class="mb-0" style="color: var(--light-gray);">© 2024 Fariha Anjum Shifa</p>
+                    <p class="my-0 pt-0"><a class="" href="mailto:sadia.afrose1927@gmail.com">sadia.afrose1927@gmail.com</a></p>
 
                 </div>
 
 
                 <div class="col col-md-7 text-right">
-                    <p class="mb-0" style="color: var(--light-gray);">Quick Links</p>
+                    <p class="mb-0" style="color: var(--light-gray);"></p>
                     <p class="my-0 pt-0">
                         <a href=""><i class="mr-3 footer-icons fa-solid fa-house" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 footer-icons fa-brands fa-google-scholar" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true"></i></a>
-                        <a href="" target="_blank"><i class="footer-icons fa-brands fa-x-twitter" aria-hidden="true"></i></a>
+                        <a href="https://www.linkedin.com/in/sadia-afrose-a8a1b11b4" target="_blank"><i class="mr-3 fab footer-icons fa-linkedin" aria-hidden="true" style="color: var(--blue-medium)"></i></a>
+                        <a href="https://github.com/sadiamimi" target="_blank"><i class="mr-3 fab footer-icons fa-github" aria-hidden="true" style="color: black"></i></a>
                     </p>
                 </div>
             </div>
