387e8bdd2f923bac7b51a8ecd75d37de094ac5b3
Sadia Afrose <95015966+sadiamimi@users.noreply.github.com>
2025-01-02 13:46:00 +0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -28,6 +28,9 @@ class Header extends HTMLElement {
 <li class="nav-item mr-2">
   <a class="nav-link" href="/pages/publications.html">Research</a>
 </li>
+<li class="nav-item mr-2">
+  <a class="nav-link" href="/pages/projects.html">Projects</a>
+</li>
 <!-- 
 <li class="nav-item mr-2">
   <a class="nav-link" href="/pages/skills.html">Skills</a>
@@ -35,9 +38,7 @@ class Header extends HTMLElement {
 <li class="nav-item mr-2">
   <a class="nav-link" href="/pages/teaching.html">Teaching</a>
 </li>
-<li class="nav-item mr-2">
-  <a class="nav-link" href="/pages/projects.html">Projects</a>
-</li>
+
 
 <li class="nav-item mr-2">
   <a class="nav-link" href="/pages/blogposts.html">Blogs</a>
