e70dc1f12ef5e7b2dc5f4948e42cbbbf7d38585d
Sadia Afrose <95015966+sadiamimi@users.noreply.github.com>
2025-01-02 13:55:06 +0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -7,46 +7,32 @@ class Header extends HTMLElement {
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-        <h1><a class="navbar-brand" href="/">Sadia Afrose</a></h1>
-        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarCollapse" style="">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/">Home</a>
-            </li>
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/education.html">Education</a>
-            </li>
-            <li class="nav-item mr-2">
-              <a class="nav-link" href="/assets/CV/SadiaAfrose_CV.pdf">CV</a>
-            </li>
-            <li class="nav-item mr-2">
-  <a class="nav-link" href="/pages/experience.html">Experience</a>
-</li>
-<li class="nav-item mr-2">
-  <a class="nav-link" href="/pages/publications.html">Research</a>
-</li>
-<li class="nav-item mr-2">
-  <a class="nav-link" href="/pages/projects.html">Projects</a>
-</li>
-<!-- 
-<li class="nav-item mr-2">
-  <a class="nav-link" href="/pages/skills.html">Skills</a>
-</li>
-<li class="nav-item mr-2">
-  <a class="nav-link" href="/pages/teaching.html">Teaching</a>
-</li>
-
-
-<li class="nav-item mr-2">
-  <a class="nav-link" href="/pages/blogposts.html">Blogs</a>
-</li>
--->
-
-          </ul>
-        </div>
+          <h1><a class="navbar-brand" href="/">Sadia Afrose</a></h1>
+          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="navbar-collapse collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="/">Home</a>
+              </li>
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="/pages/education.html">Education</a>
+              </li>
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="/assets/CV/SadiaAfrose_CV.pdf" target="_blank">CV</a>
+              </li>
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="/pages/experience.html">Experience</a>
+              </li>
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="/pages/publications.html">Research</a>
+              </li>
+              <li class="nav-item mr-2">
+                <a class="nav-link" href="/pages/projects.html">Projects</a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </header>
     `;
